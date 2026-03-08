2072f9f13ef42253c96e8b211af86fe7e917027a
Richard Russon <rich@flatcap.org>
2026-03-08 01:41:10 +0000

    re-theme

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 # -- Project information
 
 project = 'NeoMutt'
-copyright = '2025, Richard Russon'
+copyright = '2026, Richard Russon'
 author = 'flatcap'
 
 release = '0.1'
@@ -27,12 +27,14 @@ templates_path = ['_templates']
 html_theme = 'sphinx_book_theme'
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
-html_logo = "_static/mutt-48x48.png"
+html_logo = "_static/logo.png"
+html_favicon = "_static/favicon.png"
 
 html_theme_options = {
     "max_navbar_depth": 2,
     "pygments_light_style": "gruvbox-light",
     "pygments_dark_style": "gruvbox-dark",
+    "use_download_button": False,
 }
 
 # -- Options for EPUB output
